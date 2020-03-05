9e12a464f10553f9c6481837da3cd247ffe1a467
Daniel Finca <37817126+fincamd@users.noreply.github.com>
2020-03-05 23:29:37 +0100

    Several modifications. Read commit description.
    - Modified route cards so they show a map on their header position with
    a set of points received inside props object.
    - Added missing semicolons on some files so that codacy doesn't
    complain.
    - Formatted some files.

--- src/components/routeList/RouteCard.js
+++ src/components/routeList/RouteCard.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import Card from 'react-bootstrap/Card';
+import Card, { CardHeader } from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import MyMap from '../myMap/MyMap';
+import Route from '../myMap/Route';
 
 const cardStyle = {
     width: "30rem"
@@ -9,7 +11,10 @@ const cardStyle = {
 function RouteCard(props) {
     return (
         <Card style={cardStyle} text="dark">
-            <Card.Img variant="bottom" src="" />
+            {/* <Card.Img variant="bottom" src="" /> */}
+            <Card.Header>
+                <MyMap route={new Route(props.routePoints)} zoom={13} />
+            </Card.Header>
             <Card.Body>
                 <Card.Title>{props.routeName}</Card.Title>
                 <Card.Text>{props.routeDescription}</Card.Text>
