62b5e56b30fd0a77189fbcb8924f8c5609005cb4
Daniel Finca <37817126+fincamd@users.noreply.github.com>
2020-03-05 23:41:58 +0100

    Removed unused import

--- src/components/routeList/RouteCard.js
+++ src/components/routeList/RouteCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Card, { CardHeader } from 'react-bootstrap/Card';
+import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import MyMap from '../myMap/MyMap';
 import Route from '../myMap/Route';
