6eb776ad42cdb180093aaf8e9ff5c7ded0b4efaf
Óscar Sánchez Campo <UO265078@uniovi.es>
2020-03-05 13:58:08 +0100

    working on test

--- src/components/myMap/MyMap.test.js
+++ src/components/myMap/MyMap.test.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import MyMap from './MyMap';
 import Route from './Route';
+import { render, getByTestId } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 
 test('Test add route', () => {
 
-	const points= [[43.211820, -5.787902],
+const points= [[43.211820, -5.787902],
 			[43.210796, -5.786690],
 			[43.210082, -5.785064],
 			[43.209800, -5.783841],
 			[43.210121, -5.782339],
 			[43.210379, -5.780773],
 			[43.209754, -5.777683]]; 
- const {map} = render(<MyMap route={new Route(points)} zoom= {13}  />);
+			
+	const myRoute = new Route(points);
+
+	const {getByTestId} = render(<div data-testid="test" ><MyMap  route={myRoute} zoom= {13}  /></div>);
+	 
+	 //const {getByTestId} = render(<h1 data-testid="test">test</h1>);
+	 expect(getByTestId("test")).toHaveAttribute("data-testid","test");
    
 });
  
