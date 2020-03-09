97d6865b123b328fd01156a166430fb7cb761225
Diego <damandi99@gmail.com>
2020-03-09 10:18:39 +0100

    Added NavBar as component and included log in button and link to GitHub

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
 import React from 'react';
 import './App.css';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import MyNavBar from './components/navBar/navBar';
 
 function App() {
   return (
     <div className="App">
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">Sign in</Nav.Link>
-            <Nav.Link href="#memes">Sign up</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <MyNavBar/>
       <header className="App-header">
         Bienvenido
       </header>
