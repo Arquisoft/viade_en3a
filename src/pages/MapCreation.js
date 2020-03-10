0899166c00e0a9b95a8ab810ef6e3ed56b72dc1b
Óscar Sánchez <UO265078@uniovi.es>
2020-03-10 15:40:10 +0100

    Tests added

--- src/pages/MapCreation.js
+++ src/pages/MapCreation.js
@@ -3,7 +3,7 @@ import '../App.css';
 import EditableMap from '../components/editableMap/EditableMap';
 import React, { Component } from 'react';
 import {Button, InputGroup, FormControl} from 'react-bootstrap';
-
+import 'URL';
 
 
 class MapCreation extends Component {
@@ -16,17 +16,20 @@ class MapCreation extends Component {
   render(){ 
 	return (
 			<div className="App">
-				< EditableMap ref={this.points}  />
-					<InputGroup className="mb-3">
-						<InputGroup.Prepend>
-							<InputGroup.Text id="basic-addon1">Route Name</InputGroup.Text>
-						</InputGroup.Prepend>
-						<FormControl
-							ref={this.routeName}
-							aria-describedby="basic-addon1"
-						/>
-					</InputGroup>
-					<Button variant="primary" onClick={() => this.save()}  >Save</Button>
+				<h1>Create your own Route</h1>
+				<EditableMap ref={this.points} role='map'  />
+				
+				<InputGroup className="mb-3">
+					<InputGroup.Prepend>
+						<InputGroup.Text id="basic-addon1">Route Name</InputGroup.Text>
+					</InputGroup.Prepend>
+					<FormControl
+						ref={this.routeName}
+						aria-describedby="basic-addon1"
+						role='title'
+					/>
+				</InputGroup>
+				<Button variant="primary" onClick={() => this.save()}  >Save</Button>
 			</div>
 			
 		);
