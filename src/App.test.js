ba1210d11b935eeacc7c06ecd66a3f91554bd6b2
jesQM <jezuquema@gmail.com>
2020-03-12 08:37:53 +0100

    Added import

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import {findRenderedDOMComponentWithClass} from "react-dom/test-utils";
 
 test('renders learn react link', () => {
   const { getByText, tree } = render(<App />);
