b01fb0fb84788cfca5d16277802a1fc5d940a9e5
jesQM <jezuquema@gmail.com>
2020-03-12 08:20:14 +0100

    Repairing broken tests

--- src/pages/MapCreation.js
+++ src/pages/MapCreation.js
@@ -27,9 +27,9 @@ class MapCreation extends Component {
 					/>
 				</InputGroup>
 				<EditableMap ref={this.points} role='map' />
-				<Button variant="primary" onClick={() => this.save()} style={{ margin: "2vh" }}>Save as json file</Button>
-        <Button variant="primary" onClick={() => this.uploadToPod()} style={{ margin: "2vh" }}>Upload To Pod</Button>
-				<Button variant="primary" onClick={() => this.viewRoutes()} style={{ margin: "2vh" }}>View Pod</Button>
+				<Button variant="primary" onClick={() => this.save()} style={{ margin: "1.5vh" }}>Save as json file</Button>
+        		<Button variant="primary" onClick={() => this.uploadToPod()} style={{ margin: "1.5vh" }}>Upload To Pod</Button>
+				<Button variant="primary" onClick={() => this.viewRoutes()} style={{ margin: "1.5vh" }}>View Pod</Button>
 			</div >
 		);
 	}
--- src/pages/MapCreation.test.js
+++ src/pages/MapCreation.test.js
@@ -14,7 +14,7 @@ test('Test create route', () => {
 	fireEvent.click(title, leftClick);
 	fireEvent.keyDown(title, { key: 'A' });
 
-	const btn = getByText('Save');
+	const btn = getByText('/save/i');
 	fireEvent.click(btn, leftClick);
 
 	expect(getByText("Create your own Route")).toBeTruthy();
