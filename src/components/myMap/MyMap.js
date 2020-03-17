b505e61692978426be1bf8916573a14314a31010
Óscar Sánchez <56643615+oscar134@users.noreply.github.com>
2020-03-17 15:49:59 +0100

    Route List Map fixed. Now it scales into the image

--- src/components/myMap/MyMap.js
+++ src/components/myMap/MyMap.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import { Map, TileLayer, Marker, Popup, Polyline } from 'react-leaflet';
+import { Map, TileLayer, Marker, Polyline, FeatureGroup } from 'react-leaflet';
 
 class MyMap extends React.Component {
-	render() {
-		const position = [this.props.route.points[0][0], this.props.route.points[0][1]];
+	constructor() {
+		super();
+		this.map =React.createRef();
+	}
 
+	setPositionScaled = (e) => {
+		this.map.current.leafletElement.fitBounds(e.target.getBounds());
+	}
+
+	render() {
 		return (
-			<Map center={position} zoom={this.props.zoom} dragging={false}
+			<Map ref={this.map} center={[0,0]} zoom={this.props.zoom} dragging={false}
 				scrollWheelZoom={false} touchZoom={false} zoomControl={false}
 				doubleClickZoom={false} style={{ width: "19.5vw", height: "15vw", margin: "0" }}>
 				< TileLayer
 					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 					attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 				/>
-				<Polyline positions={this.props.route.points} color='red' />
-				<Marker position={this.props.route.points[0]}>
-					<Popup>Start</Popup>
-				</Marker>
-				<Marker position={this.props.route.points[this.props.route.points.length - 1]}>
-					<Popup>End</Popup>
-				</Marker>
+				<FeatureGroup onAdd={this.setPositionScaled} >
+					<Polyline positions={this.props.route.points} color='red' />
+					<Marker position={this.props.route.points[0]}/>
+					<Marker position={this.props.route.points[this.props.route.points.length - 1]}/>
+				</FeatureGroup>
 			</Map >
 		);
 	}
