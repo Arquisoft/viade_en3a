82fa4da7e255e900bb4ae451cc5db2cd7869a0fd
Daniel Finca <37817126+fincamd@users.noreply.github.com>
2020-03-23 23:56:26 +0100

    Finished fixing css for Route listing. Moved tests to test folder

--- src/components/routeList/RouteList.js
+++ src/components/routeList/RouteList.js
@@ -1,39 +1,40 @@
-import React from 'react';
-import RouteCard from './RouteCard';
-
-const routeListStyle = {
-    display: "flex",
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "space-between",
-    padding: "2%"
-};
-
-
+import React from "react";
+import RouteCard from "./RouteCard";
+import CardDeck from "react-bootstrap/CardDeck";
 
 class RouteList extends React.Component {
 
     constructor(props) {
         super(props);
         this.routeManager = props.routeManager;
+        this.cardDeckSize = 3;
     }
 
-
     render() {
-       return(
-           <div className="App-header">
-                <h1>Route list</h1>
-                <div style={routeListStyle}>
-                    {this.routeManager.getRoutes().map( (r) =>
-                    <RouteCard
-                        route={r}
-                    />
+        let routes = this.routeManager.getRoutes();
+        let routesForCardDecks = [];
+        let counter = 0;
+        while (counter <= routes.length) {
+            routesForCardDecks.push(
+                <CardDeck style={{ margin: "1%" }}>
+                    {routes.slice(counter, counter + this.cardDeckSize).map((r) =>
+                        <RouteCard
+                            route={r}
+                        />
                     )}
-                </div>
-           </div>
-       );}
+                </CardDeck>
+            );
+            counter += this.cardDeckSize;
+        }
+
+        return (
+            <div className="App-header">
+                <h1>Route list</h1>
+                {routesForCardDecks.map(route => route)}
+            </div >
+        );
+    }
 
 }
-
 
 export default RouteList;
