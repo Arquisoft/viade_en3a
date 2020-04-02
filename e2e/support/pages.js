5ffda2e0c74ce535d28a3d379be73d0f11cb5f6e
Nicolás Mencía <nicomencia4@gmail.com>
2020-04-02 08:42:07 +0200

    Add cucumber+puppeteer skeletond and actions/pages/selectors for future

---
+++ e2e/support/pages.js
@@ -0,0 +1,11 @@
+const pages = {
+  home: '#/home',
+  profile: '#/profile',
+  friends: '#/friends',
+  routesList: '#/routes/list',
+  routesList: '#/routes/list',
+  routesExample: '#/routes/example',
+  editProfile: '#/editProfile'
+};
+
+module.exports = pages;
