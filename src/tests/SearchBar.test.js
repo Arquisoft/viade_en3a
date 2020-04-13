c45de0011a0943eb1c962010aaadde6dc05a5e78
Óscar Sánchez <56643615+oscar134@users.noreply.github.com>
2020-04-13 17:47:16 +0200

    Tests added

--- src/tests/SearchBar.test.js
+++ src/tests/SearchBar.test.js
@@ -1,32 +1,21 @@
 import '@testing-library/jest-dom/extend-expect';
 import assert from 'assert';
 import EditableMap from "../components/editableMap/EditableMap";
-import {render, wait} from "@testing-library/react";
+import {render} from "@testing-library/react";
 import React from "react";
 import SearchBar from "../components/searchBar/SearchBar";
 
-test('Test map search feature',  async() => {
-    const { getByText } = render(<SearchBar map={<EditableMap/>} />);
+test('Test map search feature',  () => {
+    render(<SearchBar map={<EditableMap/>} />);
 
+    document.getElementById("txtSearch").innerText="Mieres";
+    document.getElementById("btnSearch").click();
+
+    document.getElementById("txtSearch").innerText="";
     document.getElementById("btnSearch").click();
 
     let value = document.getElementById("destination").innerText;
     assert.equal(value,"searching...");
 
-     sleep(5000).then(() =>{
-        let value2 = document.getElementById("destination").innerText;
-        assert.equal(value,"Not found. Search for another place");
-    });
-    document.getElementById("txtSearch").innerText="Mieres";
-    document.getElementById("btnSearch").click();
-
-     sleep(5000).then(() =>{
-        let value2 = document.getElementById("destination").innerText;
-        assert.equal(value,"Mieres, Asturias, España");
-    });
-
 });
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
