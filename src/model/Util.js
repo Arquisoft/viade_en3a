9d2ac2d08c3063e104aac3e367ddc9037c312bc8
Nico Mencía <nicomencia4@gmail.com>
2020-04-23 03:02:58 +0200

    Codacy fix

--- src/model/Util.js
+++ src/model/Util.js
@@ -7,57 +7,55 @@ class UserDetails {
 
     static async getName(){
         var session = await auth.currentSession();
-        var username = data[session.webId].vcard_fn
+        var username = data[session.webId].vcard_fn;
         return username;
     }
 
     static async getLocality(){
         return new Promise((resolve, reject) => {
             data.user["http://www.w3.org/2006/vcard/ns#hasAddress"].value
-              .then(addressCard => {
+              .then((addressCard) => {
                 resolve(data[addressCard].vcard_locality.value)
-                  .catch(error => reject(error));
               })
-              .catch(error => reject(error));
+              .catch((error) => reject(error));
         });
     }
 
     static async getRegion(){
         return new Promise((resolve, reject) => {
             data.user["http://www.w3.org/2006/vcard/ns#hasAddress"].value
-              .then(addressCard => {
+              .then((addressCard) => {
                 resolve(data[addressCard].vcard_region.value)
-                  .catch(error => reject(error));
               })
-              .catch(error => reject(error));
+              .catch((error) => reject(error));
         });
     }
 
     static async getEmail(){
         return new Promise((resolve, reject) => {
             data.user["http://www.w3.org/2006/vcard/ns#hasEmail"].value
-              .then(emailCard => {
+              .then((emailCard) => {
                 data[emailCard].vcard_value.value
-                  .then(email => {
+                  .then((email) => {
                     resolve(email.substring('mailto:'.length));
                   })
-                  .catch(error => reject(error));
+                  .catch((error) => reject(error));
               })
-              .catch(error => reject(error));
+              .catch((error) => reject(error));
         });
     }
 
     static async getPhone(){
         return new Promise((resolve, reject) => {
             data.user["http://www.w3.org/2006/vcard/ns#hasTelephone"].value
-              .then(phoneCard => {
+              .then((phoneCard) => {
                 data[phoneCard].vcard_value.value
-                  .then(phone => {
+                  .then((phone) => {
                     resolve(phone.substring('tel:'.length));
                   })
-                  .catch(error => reject(error));
+                  .catch((error) => reject(error));
               })
-              .catch(error => reject(error));
+              .catch((error) => reject(error));
         });
     }
 
