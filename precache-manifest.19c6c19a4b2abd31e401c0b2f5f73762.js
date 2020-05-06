507fbd7021d947599eccabbea65859fc422ce06d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-06 23:30:17 +0000

    Deploy Arquisoft/viade_en3a to github.com/Arquisoft/viade_en3a.git:gh-pages

---
+++ precache-manifest.19c6c19a4b2abd31e401c0b2f5f73762.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc88ce749c7c2b9508fe5e3cd53ee16d",
+    "url": "/viade_en3a/index.html"
+  },
+  {
+    "revision": "7c63473562e382991e10",
+    "url": "/viade_en3a/static/css/2.e5b99fdb.chunk.css"
+  },
+  {
+    "revision": "9c167b2ecb8a050f992e",
+    "url": "/viade_en3a/static/css/main.cec0b682.chunk.css"
+  },
+  {
+    "revision": "7c63473562e382991e10",
+    "url": "/viade_en3a/static/js/2.8318b827.chunk.js"
+  },
+  {
+    "revision": "af9f664e7fd4e83a68cd73f9cfea5f86",
+    "url": "/viade_en3a/static/js/2.8318b827.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c167b2ecb8a050f992e",
+    "url": "/viade_en3a/static/js/main.f44a17c1.chunk.js"
+  },
+  {
+    "revision": "0ef98ab1311a97b98a5f",
+    "url": "/viade_en3a/static/js/runtime-main.382c944d.js"
+  },
+  {
+    "revision": "5c6dfa65943093ae44e7e316ac553aff",
+    "url": "/viade_en3a/static/media/Screenshot_1.5c6dfa65.png"
+  },
+  {
+    "revision": "36cf0aced274ccbd9d10972cfd56d04a",
+    "url": "/viade_en3a/static/media/Screenshot_4.36cf0ace.png"
+  },
+  {
+    "revision": "774f104ca940d2f5552017963355d94f",
+    "url": "/viade_en3a/static/media/car_img1.774f104c.jpeg"
+  },
+  {
+    "revision": "67c42e97ffed6ed12c9b53d927b2c4d5",
+    "url": "/viade_en3a/static/media/car_img2.67c42e97.jpg"
+  },
+  {
+    "revision": "4bb0f0c0c7f303f042a6337281d29c4b",
+    "url": "/viade_en3a/static/media/iconEdited.4bb0f0c0.svg"
+  },
+  {
+    "revision": "7350e1f94f8bc59f7b4d58d54ef95c4f",
+    "url": "/viade_en3a/static/media/iconNormal.7350e1f9.svg"
+  },
+  {
+    "revision": "808b19f1c4b4632d6ba0130ac9339597",
+    "url": "/viade_en3a/static/media/iconSelected.808b19f1.svg"
+  },
+  {
+    "revision": "7e2fd07534e327c542e1d56a64287da2",
+    "url": "/viade_en3a/static/media/logo.7e2fd075.jpeg"
+  },
+  {
+    "revision": "bdb0bd56f5e194f4457c8ac441a54b66",
+    "url": "/viade_en3a/static/media/logo_alt.bdb0bd56.jpeg"
+  },
+  {
+    "revision": "ddf87e7e336ea0a4e1bc6577584f67ac",
+    "url": "/viade_en3a/static/media/logo_full.ddf87e7e.jpeg"
+  },
+  {
+    "revision": "c6e6636fe9d430fe7a93a70157c26060",
+    "url": "/viade_en3a/static/media/logo_letters.c6e6636f.jpeg"
+  },
+  {
+    "revision": "7e2fd07534e327c542e1d56a64287da2",
+    "url": "/viade_en3a/static/media/logo_old.7e2fd075.jpeg"
+  },
+  {
+    "revision": "06374f0372d2e9f972b441283c9e8339",
+    "url": "/viade_en3a/static/media/main.06374f03.png"
+  },
+  {
+    "revision": "0a843bdf53a17081612c52a711ad538f",
+    "url": "/viade_en3a/static/media/newRouteList.0a843bdf.png"
+  },
+  {
+    "revision": "2a1e5f72d73d4896be327ecdae776d43",
+    "url": "/viade_en3a/static/media/routeDetails.2a1e5f72.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/viade_en3a/precache-manifest.19c6c19a4b2abd31e401c0b2f5f73762.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/viade_en3a/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
